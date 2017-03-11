4a7168a17c6fb0ffc1b7f6f05155af909b139834
zhengjianglong <zhengjianglong@meituan.com>
2017-03-11 11:06:50 +0800

    webpack 1,2

--- plugin.js
+++ plugin.js
@@ -1,44 +1,72 @@
 var path = require('path');
 var postcss = require('postcss');
-var RawSource = require('webpack/lib/RawSource');
+var RawSource = require('webpack-sources').RawSource;
 
-function DedupCSSPlugin(opts) {
-  var options = this.options = {};
-  opts = opts || {};
-  options.suffix = !!opts.override ? '' : (opts.suffix || '');
+function DedupCSSPlugin (options) {
+    this.options = options || {
+        removeComments: true
+    };
 }
 
-function deduplicate(root) {
-  root.each(function (node) {
-    if (node.nodes) deduplicate(node);
-  });
-
-  root.each(function (node) {
-    if (node.type === 'comment') return;
-
-    var nodeStr = node.toString();
-    while (node = node.next()) {
-      if (node.toString() === nodeStr) {
-        node = [node.prev(), node.remove()][0];
-      }
+function dedup (root, options) {
+    function isNodeEqual (a, b) {
+        var isSameShape = ['type', 'important', 'name', 'selector', 'params', 'prop', 'value'].every(function (i) {
+            return a[i] === b[i];
+        });
+        if (isSameShape && a.nodes) {
+            return a.nodes.length === b.nodes.length && a.nodes.every(function (item, index) {
+                return isNodeEqual(item, b.nodes[index]);
+            });
+        }
+        return isSameShape;
     }
-  });
+    function removeComments (root) {
+        root.walkComments(function (comment) {
+            comment.remove();
+        });
+    }
+    function dedupSiblings (node) {
+        var precedent = node;
+        while (precedent = precedent.prev()) {
+            if (isNodeEqual(precedent, node)) {
+                precedent = [precedent.next(), precedent.remove()][0];
+            }
+        }
+        if (node.prev()) {
+            dedupSiblings(node.prev());
+        }
+    }
+    function digest (node) {
+        if (!node || !node.nodes) {
+            return node;
+        }
+        node.each(digest);
+        if (node.last) {
+            dedupSiblings(node.last);
+        }
+        return node;
+    }
+
+    if (options.removeComments) {
+        removeComments(root);
+    }
+    return digest(root);
 }
 
-DedupCSSPlugin.prototype.apply = function(compiler) {
-  var suffix = this.options.suffix;
-  compiler.plugin('emit', function(compilation, cb) {
-    var assets = compilation.assets;
-    Object.keys(assets).forEach(function(assetName) {
-      if (path.extname(assetName) === '.css') {
-        var asset = assets[assetName];
-        var root = postcss.parse(asset.source(), { map: { prev: false }});
-        deduplicate(root);
-        assets[assetName + suffix] = new RawSource(root.toResult().css);
-      }
+DedupCSSPlugin.prototype.apply = function (compiler) {
+    var options = this.options;
+    compiler.plugin('emit', function (compilation, callback) {
+        var assets = compilation.assets;
+        Object.keys(assets).forEach(function(assetName) {
+            if (path.extname(assetName) === '.css') {
+                var asset = assets[assetName];
+                var source = postcss.parse(asset.source());
+                var deduped = dedup(source, options);
+                assets[assetName] = new RawSource(deduped.toResult().css);
+            }
+        });
+        callback();
     });
-    cb();
-  });
 };
 
 module.exports = DedupCSSPlugin;
